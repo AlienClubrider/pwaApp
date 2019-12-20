733e340d7ad7d4152e4f7a0c3f65234e1ef3a5c2
Bryson Bailey <psp1881@yahoo.com>
2019-12-19 18:20:21 -0600

    redirect

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/pwaApp'));
-app.enable("trust proxy");
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/pwaApp/index.html'));
+app.use(function(request, response) {
+    if (!request.secure) {
+        res.sendFile(path.join(__dirname + '/dist/pwaApp/index.html'));
+    }
 });
 
 // Start the app by listening on the default Heroku port
