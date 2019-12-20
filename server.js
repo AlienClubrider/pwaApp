9e01849a8e8d29c855f0b7cd04b9775eb2b984c8
Bryson Bailey <psp1881@yahoo.com>
2019-12-19 18:15:18 -0600

    redirct

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const path = require('path');
 
 const app = express();
 
-var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
-
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/pwaApp'));
-app.use(redirectToHTTPS);
+app.enable("trust proxy");
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/pwaApp/index.html'));
