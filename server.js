00afae2e587bd0c15722aebe3cc8f0ab91fde4ff
Bryson Bailey <psp1881@yahoo.com>
2019-12-19 18:11:49 -0600

    redirct

--- server.js
+++ server.js
@@ -4,17 +4,15 @@ const path = require('path');
 
 const app = express();
 
+var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/pwaApp'));
+app.use(redirectToHTTPS);
 
-// set up a route to redirect http to https
-http.get('*', function(req, res) {
-    // res.sendFile(path.join(__dirname + '/dist/pwaApp/index.html'));
-    res.redirect('https://' + req.headers.host + req.url);
-
-    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
-    // res.redirect('https://example.com' + req.url);
-})
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/pwaApp/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
